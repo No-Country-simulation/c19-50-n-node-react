bd9f940e6219c9d736c4bb6d1261b9d0fb4abd53
jp-quintana <juanquintana1996@gmail.com>
2024-07-24 00:25:52 -0300

    refactor & add routes

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div className="">
+      <h1>¡BIENVENIDOS!</h1>
+      <p className="m-2">soy la pagina inicial</p>
+    </div>
+  );
+};
+
+export default HomePage;
