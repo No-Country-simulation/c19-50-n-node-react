a1eaa6eb5dd0e9a24517064158a99b43d1633468
jp-quintana <juanquintana1996@gmail.com>
2024-07-26 01:44:46 -0300

    wip: global user state

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,116 +1,5 @@
-// import { useForm } from 'react-hook-form';
-// import { zodResolver } from '@hookform/resolvers/zod';
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from '@/components/ui/form';
-// import { Input } from '@/components/ui/input';
-// import { Button } from '@/components/ui/button';
-// import PasswordInput from './PasswordInput';
-// import { registerSchema } from '@/schemas/registerSchema';
-
-// const RegisterForm = () => {
-//   const form = useForm({
-//     resolver: zodResolver(registerSchema),
-//     defaultValues: {
-//       name: '',
-//       lastName: '',
-//       email: '',
-//       password: '',
-//       confirmPassword: '',
-//     },
-//   });
-
-//   const onSubmit = async (values) => {
-//     console.log(values);
-//   };
-
-//   return (
-//     <Form {...form}>
-//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//         <FormField
-//           control={form.control}
-//           name="name"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Name</FormLabel>
-//               <FormControl>
-//                 <Input placeholder="Name" {...field} />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <FormField
-//           control={form.control}
-//           name="lastName"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Last name</FormLabel>
-//               <FormControl>
-//                 <Input placeholder="Last name" {...field} />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <FormField
-//           control={form.control}
-//           name="email"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Email</FormLabel>
-//               <FormControl>
-//                 <Input placeholder="youraddress@email.com" {...field} />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <FormField
-//           control={form.control}
-//           name="password"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Password</FormLabel>
-//               <FormControl>
-//                 <PasswordInput {...field} />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <FormField
-//           control={form.control}
-//           name="confirmPassword"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Confirm password</FormLabel>
-//               <FormControl>
-//                 <PasswordInput {...field} />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <div className="flex justify-center">
-//           <Button className="text-center w-full" type="submit">
-//             Submit
-//           </Button>
-//         </div>
-//       </form>
-//     </Form>
-//   );
-// };
-
-// export default RegisterForm;
-
 import { useState } from 'react';
-
+import { userStore } from '@/store/user';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
@@ -129,6 +18,8 @@ import PasswordInput from './PasswordInput';
 import { registerSchema } from '@/lib/schemas/registerSchema';
 
 const RegisterForm = () => {
+  const { setUser } = userStore((state) => state);
+
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm({
@@ -144,7 +35,12 @@ const RegisterForm = () => {
 
   const onSubmit = (values) => {
     setIsLoading(true);
-    console.log(values);
+    setUser({
+      email: values.email,
+      name: values.name,
+      lastName: values.lastName,
+      token: 'abc',
+    });
     setIsLoading(false);
   };
 
