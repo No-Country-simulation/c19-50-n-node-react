527f2da65ebaecf4fb72e3e18b021bebb24d4638
Leandro Piazza <leandro.piazza.dev@gmail.com>
2024-07-26 23:45:53 -0300

    Fix: error AuthModule

--- server/src/questions/questions.module.ts
+++ server/src/questions/questions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import {Questions} from "./questions.entity";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {QuestionsController} from "./questions.controller";
-import {QuestionsService} from "./questions.service";
+import { Questions } from './questions.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuestionsController } from './questions.controller';
+import { QuestionsService } from './questions.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Questions])],
-    controllers: [QuestionsController ],
-    providers: [QuestionsService],
+  imports: [TypeOrmModule.forFeature([Questions]), AuthModule],
+  controllers: [QuestionsController],
+  providers: [QuestionsService],
 })
 export class QuestionsModule {}
