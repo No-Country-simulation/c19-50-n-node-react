7bc711aa1191f3b489dd15df02d1c013a1328d0e
jp-quintana <juanquintana1996@gmail.com>
2024-07-26 23:19:11 -0300

    update header ui

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,29 +1,46 @@
 import MaxWidthContainer from './MaxWidthContainer';
 
 import { userStore } from '@/store/user';
-import { Button } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 
 const Nav = () => {
   const { user, removeUser } = userStore((state) => state);
   return (
-    <div className="fixed w-full flex justify-center bg-background">
+    <div className="fixed w-full flex justify-center glassmorphism py-3">
       <MaxWidthContainer>
         <nav className="flex justify-between items-center">
-          <a href="/">Descubre Cordoba</a>
+          <a className="font-bold text-xl" href="/">
+            Descubre Cordoba
+          </a>
           <ul className="flex gap-x-3 items-center">
             {!user ? (
               <>
                 <li>
-                  <a href="/login">Ingresar</a>
+                  <a
+                    href="/login"
+                    className={buttonVariants({ variant: 'outline' })}
+                  >
+                    Ingresar
+                  </a>
                 </li>
                 <li>
-                  <a href="/register">Crear cuenta</a>
+                  <a
+                    href="/register"
+                    className={buttonVariants({ variant: 'default' })}
+                  >
+                    Crear cuenta
+                  </a>
                 </li>
               </>
             ) : (
               <>
                 <li>
-                  <a href="/profile">Mi perfil</a>
+                  <a
+                    href="/profile"
+                    className={buttonVariants({ variant: 'outline' })}
+                  >
+                    Mi perfil
+                  </a>
                 </li>
                 <li>
                   <Button onClick={removeUser}>Logout</Button>
