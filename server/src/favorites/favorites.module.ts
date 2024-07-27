9ad4929c9b7bab1061c81e36e5be0b82cf75bcb8
Leandro Piazza <leandro.piazza.dev@gmail.com>
2024-07-26 23:33:33 -0300

    Fix: error module AuthGuard

--- server/src/favorites/favorites.module.ts
+++ server/src/favorites/favorites.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import {Favorites} from "./favorites.entity";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {FavoritesController} from "./favorites.controller";
-import {FavoritesService} from "./favorites.service";
+import { Favorites } from './favorites.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Favorites]) ],
-    controllers: [FavoritesController],
-    providers: [FavoritesService],
+  imports: [TypeOrmModule.forFeature([Favorites]), AuthModule],
+  controllers: [FavoritesController],
+  providers: [FavoritesService],
 })
 export class FavoritesModule {}
