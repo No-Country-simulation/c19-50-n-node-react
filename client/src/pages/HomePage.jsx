c15801c7edb0bc69fd15bdb75cb53733da9b9520
jp-quintana <juanquintana1996@gmail.com>
2024-08-01 00:08:13 -0300

    fix props

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import MaxWidthContainer from '@/components/MaxWidthContainer';
 import PostCardCarousel from '@/components/PostCardCarousel';
-import POSTS from '@/data/posts';
 
 const HomePage = () => {
   return (
@@ -8,7 +7,7 @@ const HomePage = () => {
       <section className="w-full">
         <MaxWidthContainer>
           <p className="font-bold text-3xl mb-3">Próximos eventos</p>
-          <PostCardCarousel posts={POSTS} />
+          <PostCardCarousel />
         </MaxWidthContainer>
       </section>
     </div>
