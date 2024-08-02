32c1c4cb7705bb75fe20d179b3367f4006f978b2
jp-quintana <juanquintana1996@gmail.com>
2024-08-02 01:57:43 -0300

    add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,40 @@
+import { Link } from 'react-router-dom';
+import MaxWidthContainer from './MaxWidthContainer';
+import { Separator } from './ui/separator';
+import { FacebookIcon, Instagram, Twitter } from 'lucide-react';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-foreground text-background">
+      <MaxWidthContainer className="py-10">
+        <div>
+          <div className="grid grid-cols-4 gap-10 justify-between items-center max-md:grid-cols-2">
+            <div>
+              <Link className="font-bold text-xl" to="/">
+                Descubre Cordoba
+              </Link>
+              <p>Lorem ipsum elit, officiis repellendus delectus adipisci.</p>
+            </div>
+            {[...Array(3)].map((_, i) => (
+              <ul key={i}>
+                <li className="font-bold">Lorem</li>
+                <li>Lorem ipsum dolor sit</li>
+                <li>Lorem ipsum dolor sit</li>
+                <li>Lorem ipsum dolor sit</li>
+              </ul>
+            ))}
+          </div>
+
+          <Separator className="my-6" />
+          <div className="flex justify-end max-md:justify-center gap-3">
+            <Instagram />
+            <FacebookIcon />
+            <Twitter />
+          </div>
+        </div>
+      </MaxWidthContainer>
+    </div>
+  );
 };
 
 export default Footer;
